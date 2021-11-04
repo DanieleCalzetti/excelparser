42bedbb8855125bacda9419c4b88d50f40f9ea06
calzettidaniele <calzettidaniele@gmail.com>
2021-11-04 11:36:32 +0100

    Resolved issues #3 #2 #1

--- main.py
+++ main.py
@@ -1,81 +1,146 @@
+from tkinter.constants import END
 from openpyxl import load_workbook
-#import xlwings
 import sys
-from tkinter import *
-from tkinter.ttk import *
+import tkinter as tk
+from tkinter.constants import END
 from tkinter.filedialog import askopenfilename
-import time
 
 sys.path.append(".")
 
 from read_document import read_document 
 
-ws = Tk()
+# TODO:
+# Understand classes 
+# Understand methods vs functions
+# Understand importing from other files 
+
+ws = tk.Tk()
 ws.title('Excel Parser')
-ws.geometry('700x700') 
+ws.geometry('900x900') 
 
-label_0=Label(ws,text="Insert the column value", width=20,font=("bold",20))
-label_0.place(x=200,y=60)
+top_title = tk.Frame(ws)
+values = tk.Frame(ws)
+middle_title = tk.Frame(ws)
+results = tk.Frame(ws)
 
-coverage_label=Label(ws,text="Coverage", width=20,font=("bold",10))
-coverage_label.place(x=180,y=130)
+ws.grid_rowconfigure(3, weight=1)
+ws.grid_columnconfigure(0, weight=1)
 
-coverage_entry=Entry(ws)
+top_title.grid(row=0, sticky="")
+values.grid(row=1, sticky="N")
+middle_title.grid(row=2, sticky="N")
+results.grid(row=3, sticky="N")
+
+# Top title GUI
+
+title=tk.Label(top_title,text="Insert the column value", width=20, pady=10, font=("bold",20))
+title.grid(row=0, column=0)
+
+# Values GUI
+
+coverage_label=tk.Label(values,text="Coverage", anchor="w", width=22, pady=5, font=("bold",10))
+coverage_label.grid(row=0, column=0)
+
+coverage_entry=tk.Entry(values)
 coverage_entry.insert(END, 'Copertura')
-coverage_entry.place(x=340,y=130)
+coverage_entry.grid(row=0, column=1)
 
-impression_label=Label(ws,text="Impression", width=20,font=("bold",10))
-impression_label.place(x=180,y=170)
+impression_label=tk.Label(values,text="Impression", anchor="w", width=22, pady=5, font=("bold",10))
+impression_label.grid(row=1, column=0)
 
-impression_entry=Entry(ws)
+impression_entry=tk.Entry(values)
 impression_entry.insert(END, 'Impression')
-impression_entry.place(x=340,y=170)
+impression_entry.grid(row=1, column=1 )
 
-likes_label=Label(ws,text="Likes", width=20,font=("bold",10))
-likes_label.place(x=180,y=210)
+likes_label=tk.Label(values,text="Likes", anchor="w", width=22, pady=5, font=("bold",10))
+likes_label.grid(row=2, column=0)
 
-likes_entry=Entry(ws)
+likes_entry=tk.Entry(values)
 likes_entry.insert(END, 'Likes')
-likes_entry.place(x=340,y=210)
+likes_entry.grid(row=2, column=1 )
 
-hashtag_label=Label(ws,text="Hashtag", width=20,font=("bold",10))
-hashtag_label.place(x=180,y=250)
+hashtag_label=tk.Label(values,text="Hashtag", anchor="w", width=22, pady=5, font=("bold",10))
+hashtag_label.grid(row=3, column=0)
 
-hashtag_entry=Entry(ws)
+hashtag_entry=tk.Entry(values)
 hashtag_entry.insert(END, 'Hashtag')
-hashtag_entry.place(x=340,y=250)
+hashtag_entry.grid(row=3, column=1 )
 
-first_post_label=Label(ws,text="Row of the first post", width=20,font=("bold",10))
-first_post_label.place(x=180,y=290)
+first_post_label=tk.Label(values,text="Row of the first post", anchor="w", width=22, pady=5, font=("bold",10))
+first_post_label.grid(row=4, column=0)
 
-first_post_entry=Entry(ws)
+first_post_entry=tk.Entry(values)
 first_post_entry.insert(END, 10)
-first_post_entry.place(x=340,y=290)
+first_post_entry.grid(row=4, column=1 )
 
-header_row_label=Label(ws,text="Header row", width=20,font=("bold",10))
-header_row_label.place(x=180,y=330)
+header_row_label=tk.Label(values,text="Header row", anchor="w", width=22, pady=5, font=("bold",10))
+header_row_label.grid(row=5, column=0)
 
-header_row_entry=Entry(ws)
+header_row_entry=tk.Entry(values)
 header_row_entry.insert(END, 8)
-header_row_entry.place(x=340,y=330)
+header_row_entry.grid(row=5, column=1)
 
-excel_label = Label(ws, text='Upload Excel file', width=20,font=("bold",10))
-excel_label.place(x=180, y=410)
+excel_label = tk.Label(values, text='Upload Excel file', anchor="w", width=22, pady=5, font=("bold",10))
+excel_label.grid(row=6, column=0)
 
-excel_button = Button(ws, text ='Choose File and get column values', command = lambda:open_file()) 
-excel_button.place(x=340, y=410)
+excel_button = tk.Button(values, text ='Choose File and get column values', command = lambda:open_file())
+excel_button.grid(row=6, column=1)
 
-common_hashtag_label = Label(ws, text = 'The commons hashtag are:', font=("bold",10))
-common_hashtag_label.place(x=180, y=450)
+# Middle title GUI
 
-highest_coverage_label = Label(ws, text = 'The post with the highest coverage is at:' ,font=("bold",10))
-highest_coverage_label.place(x=180, y=490)
+results_title = tk.Label(middle_title,text="Values returned", width=20, pady=10, font=("bold",20))
+results_title.grid(row=0, column=0)
 
-highest_impression_label = Label(ws, text = 'The post with the highest value of impression is at:', font=("bold",10))
-highest_impression_label.place(x=180, y=530)
+# Results GUI
 
-highest_likes_label = Label(ws, text = 'The post with the highest number of likes is at:', font=("bold",10))
-highest_likes_label.place(x=180, y=570)
+coordinate_top_three_cov = tk.Label(results, text = 'The three posts with highest coverage are:', anchor="w", width=66, pady=5, font=("bold",10))
+coordinate_top_three_cov.grid(row=0, column=0)
+
+coordinate_top_three_cov_entry=tk.Entry(results)
+coordinate_top_three_cov_entry.insert(END, "")
+coordinate_top_three_cov_entry.grid(row=0, column=1)
+
+coordinate_top_three_like = tk.Label(results, text = 'The three posts with highest likes are:', anchor="w", width=66, pady=5, font=("bold",10))
+coordinate_top_three_like.grid(row=1, column=0)
+
+coordinate_top_three_like=tk.Entry(results)
+coordinate_top_three_like.insert(END, "")
+coordinate_top_three_like.grid(row=1, column=1)
+
+coordinate_top_three_impre = tk.Label(results, text = 'The three posts with highest impression are:', anchor="w", width=66, pady=5, font=("bold",10))
+coordinate_top_three_impre.grid(row=2, column=0)
+
+coordinate_top_three_impre=tk.Entry(results)
+coordinate_top_three_impre.insert(END, "")
+coordinate_top_three_impre.grid(row=2, column=1)
+
+common_hashtag_cov = tk.Label(results, text = 'The commons hashtags for the top three with highest coverage are:', anchor="w", width=66, pady=5, font=("bold",10))
+common_hashtag_cov.grid(row=3, column=0)
+
+common_hashtag_cov=tk.Entry(results)
+common_hashtag_cov.insert(END, "")
+common_hashtag_cov.grid(row=3, column=1)
+
+common_hashtag_like = tk.Label(results, text = 'The commons hashtags for the top three with highest likes are:', anchor="w", width=66, pady=5, font=("bold",10))
+common_hashtag_like.grid(row=4, column=0)
+
+common_hashtag_like=tk.Entry(results)
+common_hashtag_like.insert(END, "")
+common_hashtag_like.grid(row=4, column=1)
+
+common_hashtag_impre = tk.Label(results, text = 'The commons hashtags for the top three with highest impressions are:', anchor="w", width=66, pady=5, font=("bold",10))
+common_hashtag_impre.grid(row=5, column=0)
+
+common_hashtag_impre=tk.Entry(results)
+common_hashtag_impre.insert(END, "")
+common_hashtag_impre.grid(row=5, column=1)
+
+common_hashtag_label = tk.Label(results, text = 'The commons hashtags are:', anchor="w", width=66, pady=5, font=("bold",10))
+common_hashtag_label.grid(row=6, column=0)
+
+common_hashtag_label=tk.Entry(results)
+common_hashtag_label.insert(END, "")
+common_hashtag_label.grid(row=6, column=1)
 
 def get_args():
     args = {}
@@ -89,24 +154,45 @@ def get_args():
 
 def open_file():
     file_path = askopenfilename(filetypes=[("Excel files", "*.xlsx")]) 
+    coordinate_top_three_cov_entry.delete(0, 'end')
+    coordinate_top_three_like.delete(0, 'end')
+    coordinate_top_three_impre.delete(0, 'end')
+    common_hashtag_cov.delete(0, 'end')
+    common_hashtag_like.delete(0, 'end')
+    common_hashtag_impre.delete(0, 'end')
+    common_hashtag_label.delete(0, 'end') 
     if file_path != "":
         wb = load_workbook(file_path)
         sheets = wb.sheetnames
         sheet = wb[sheets[0]]
         args = get_args()
         s = read_document(sheet)
-        s.check_row_used(args["first_post"])
         headers = s.find_header(args["coverage_header"], args["like_header"], args["row_header"], args["impression_header"], args["hashtag_header"])
         if "" in headers:
-            highest_coverage_label["text"] = 'The post with the highest coverage is at: "Error, please review your headers!"'
-            highest_impression_label["text"] = 'The post with the highest value of impression is at: "Error, please review your headers!"' 
-            highest_likes_label["text"] = 'The post with the highest number of likes is at: "Error, please review your headers!"'
+            coordinate_top_three_cov_entry.insert(0, "Error, please review your headers!")
+            coordinate_top_three_like.insert(0, "Error, please review your headers!")
+            coordinate_top_three_impre.insert(0, "Error, please review your headers!")
+            common_hashtag_cov.insert(0, "Error, please review your headers!")
+            common_hashtag_like.insert(0, "Error, please review your headers!")
+            common_hashtag_impre.insert(0, "Error, please review your headers!")
+            common_hashtag_label.insert(0, "Error, please review your headers!")            
         else:
-            rows = s.find_top_post()
-            highest_coverage_label["text"] = "".join(['The post with the highest coverage is at: A', str(rows[0])])
-            highest_impression_label["text"] = "".join(['The post with the highest value of impression is at: A', str(rows[1])])
-            highest_likes_label["text"] = "".join(['The post with the highest number of likes is at: A', str(rows[2])])   
-            common_hashtag_label["text"] = "".join(["The commons hashtag are: ", str(s.compare_hashtag())])
+            s.check_row_used(args["first_post"])
+            top_posts = s.find_top_post()
+            hashtag_top_three = s.compare_hashtag_top_three()
+            common_global_hashtag = s.compare_hashtag()
+            top_posts_cov_list = list(top_posts[0])
+            top_posts_like_list = list(top_posts[1])
+            top_posts_impre_list = list(top_posts[2])
+
+            coordinate_top_three_cov_entry.insert(0, "".join(["A", top_posts_cov_list[0], ", ", "A", top_posts_cov_list[1], ", ", "A", top_posts_cov_list[2]]))
+            coordinate_top_three_like.insert(0,  "".join(["A", top_posts_like_list[0], ", ", "A", top_posts_like_list[1], ", ", "A", top_posts_like_list[2]]))
+            coordinate_top_three_impre.insert(0, "".join(["A", top_posts_impre_list[0], ", ", "A", top_posts_impre_list[1], ", ", "A", top_posts_impre_list[2]]))
+
+            common_hashtag_cov.insert(0, " ".join(hashtag_top_three[0]))
+            common_hashtag_like.insert(0, " ".join(hashtag_top_three[1]))
+            common_hashtag_impre.insert(0, " ".join(hashtag_top_three[2]))
+            common_hashtag_label.insert(0, common_global_hashtag)
     else:
         pass
 
